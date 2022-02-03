23b2b0f2914641a46fbd8737a8d250146ef9dfe7
Taylor <taylorwhite21@yahoo.com>
2022-02-02 23:12:50 -0500

    testing API

--- api/giveaways.py
+++ api/giveaways.py
@@ -18,7 +18,7 @@ class handler(BaseHTTPRequestHandler):
       data = r.json()
       games = []
       for game_data in data:
-        giveaways = game_data['platforms']
+        giveaways = game_data['title']['platforms']['open_giveaway_url']['status']
         games.append(giveaways)
       message = str(games)
     else:
