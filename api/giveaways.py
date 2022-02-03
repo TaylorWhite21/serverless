d3256a80f82a8372908adae683193d25cbc4de35
Taylor <taylorwhite21@yahoo.com>
2022-02-02 22:12:20 -0500

    testing API

--- api/giveaways.py
+++ api/giveaways.py
@@ -10,12 +10,25 @@ class handler(BaseHTTPRequestHandler):
     # self.end_headers()
     # message = 'Hello World!' 
     # self.wfile.write(message.encode())
-    url_path = self.path
-    url_components = parse.urlsplit(url_path)
-    query_string_list = parse.parse_qsl(url_components.query)
-    dic = dict(query_string_list)
-    # GET https://www.gamerpower.com/api/giveaways?platform=pc
-    if "platforms" in dic:
-      url = 'https://www.gamerpower.com/api/giveaways'
-      r = requests.get(url + dic['platforms'])
-      print(r)
+    # url_path = self.path
+    # url_components = parse.urlsplit(url_path)
+    # query_string_list = parse.parse_qsl(url_components.query)
+    # dic = dict(query_string_list)
+    # # GET https://www.gamerpower.com/api/giveaways?platform=pc
+    # if "platforms" in dic:
+    #   url = 'https://www.gamerpower.com/api/giveaways'
+    #   r = requests.get(url + dic['platforms'])
+    #   print(r)
+
+    url = "https://gamerpower.p.rapidapi.com/api/giveaways"
+    
+    querystring = {"platform":"xbox"}
+    
+    headers = {
+        'x-rapidapi-host': "gamerpower.p.rapidapi.com",
+        'x-rapidapi-key': "20db20870fmshe463a252e22f924p103b22jsn8088621c31c8"
+        }
+    
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    
+    print(response.text)
