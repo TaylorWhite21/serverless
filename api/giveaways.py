fbca4977af1d398c289c0718e52dbb5f1063e2db
Taylor <taylorwhite21@yahoo.com>
2022-02-02 22:32:23 -0500

    testing API

--- api/giveaways.py
+++ api/giveaways.py
@@ -5,30 +5,61 @@ import requests
 class handler(BaseHTTPRequestHandler):
   
   def do_GET(self):
-    # self.send_response(200)
-    # self.send_header('Content-type', 'text/plain')
-    # self.end_headers()
-    # message = 'Hello World!' 
-    # self.wfile.write(message.encode())
-    # url_path = self.path
-    # url_components = parse.urlsplit(url_path)
-    # query_string_list = parse.parse_qsl(url_components.query)
-    # dic = dict(query_string_list)
-    # # GET https://www.gamerpower.com/api/giveaways?platform=pc
-    # if "platforms" in dic:
-    #   url = 'https://www.gamerpower.com/api/giveaways'
-    #   r = requests.get(url + dic['platforms'])
-    #   print(r)
+    url_path = self.path
+    url_components = parse.urlsplit(url_path)
+    query_string_list = parse.parse_qsl(url_components.query)
+    dic = dict(query_string_list)
+    # GET https://www.gamerpower.com/api/giveaways?platform=pc
+    r = requests.get('https://www.gamerpower.com/api/giveaways')
+    print(r.json())
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    return
 
-    url = "https://gamerpower.p.rapidapi.com/api/giveaways"
-    
-    querystring = {"platform":"xbox"}
-    
-    headers = {
-        'x-rapidapi-host': "gamerpower.p.rapidapi.com",
-        'x-rapidapi-key': "20db20870fmshe463a252e22f924p103b22jsn8088621c31c8"
-        }
-    
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    
-    print(response.text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# url_path = self.path
+#     url_components = parse.urlsplit(url_path)
+#     query_string_list = parse.parse_qsl(url_components.query)
+#     dic = dict(query_string_list)
+
+#     if "word" in dic:
+#         url = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
+#         r = requests.get(url + dic['word'])
+#         data = r.json()
+#         definitions = []
+#         for word_data in data:
+#             definition = word_data["meanings"][0]["definitions"][0]["definition"]
+#             definitions.append(definition)
+#         message = str(definitions)        
+#     else:
+#         message = "Please give me a word to define"
+
+#     self.send_response(200)
+#     self.send_header('Content-type', 'text/plain')
+#     self.end_headers()
+
+#     self.wfile.write(message.encode())
+
+#     return
